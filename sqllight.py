61283d206f1237ac19bfd01b1ac634b78ba7577f
annGit001 <annagalstyan01@mail.ru>
2026-05-28 17:38:00 +0400

    update code

--- sqllight.py
+++ sqllight.py
@@ -26,45 +26,86 @@ class Manager:
     def select_all(self):
         return self.db.execute(f"SELECT * FROM {self.table}").fetchall()
 
+    def get(self,**kwargs):
+        key = list(kwargs.keys())[0]
+        value = kwargs[key]
+        sql = f"""
+            SELECT * FROM {self.table} WHERE {key} = ?
+        """
+        return self.db.execute(sql,(value,)).fetchone()
+
+    def update(self, filters: dict, **kwargs):
+        set_ = ", ".join(f"{i}=?" for i in kwargs)
+        where_ = " AND ".join(f"{i}=?" for i in filters)
+
+        values = tuple(kwargs.values()) + tuple(filters.values())
+
+        sql = f"""
+            UPDATE {self.table}
+            SET {set_}
+            WHERE {where_}
+        """
+
+        self.db.execute(sql, values)
+
+    def delete(self, **filters):
+        where_ = " AND ". join(f"{i} = ?" for i in filters)
+        values = tuple(filters.values())
+
+        sql = f"""
+            DELETE FROM {self.table}
+            WHERE {where_}
+        """
+
+        self.db.execute(sql, values)
 
 class Model:
     db = Database()
 
+    @classmethod
+    def create_table(cls):
+        fields = []
+        for key,value in cls.__dict__.items():
+            if not key.startswith('__') and isinstance(value, str):
+                fields.append(f"{key} {value}")
+        fields_sql = ", ".join(fields)
+
+        sql = f"""
+            CREATE TABLE IF NOT EXISTS {cls.__name__.lower()}s (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            {fields_sql}
+        )
+        """
+        print(sql)
+        cls.db.execute(sql)
+
+
+
+
+
 
 class User(Model):
-    pass
+    name = "TEXT"
+    age = 'INTEGER'
+    city = 'TEXT'
 
 
-
-User.objects = Manager(User.db, 'user')
-
-User.db.execute("""
-CREATE TABLE IF NOT EXISTS user (
-    id INTEGER PRIMARY KEY,
-    name TEXT UNIQUE,
-    age INTEGER
-)
-""")
-
-# user = Manager(db, 'user')
-
-# user.insert(name='Anna', age=25)
+User.objects = Manager(User.db, 'users')
+# User.create_table()
 
 
-# db.execute("""
-#     INSERT INTO user (name, age) VALUES (?, ?)
-# """,("Anna", 24))
-
-# rows = db.execute("""
-#     Select * from user
-# """).fetchall()
-# print(rows)
-# print(user.select_all())
-
-
-User.objects.insert(
-    name="Anna",
-    age=25
-)
+# User.objects.insert(
+#     name = "Anna",
+#     age = 25,
+#     city = "Yerevan"
+# )
 
 print(User.objects.select_all())
+# print(User.objects.get(name = "Anna"))
+# User.objects.update(
+#     {"name": "Anna"},
+#     age = 30,
+#     city = "Erevan"
+# )
+
+# User.objects.delete(city="Erevan")
